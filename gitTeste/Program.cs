563fb07b33f38d1496e27958593539b5fbe46ebd
elGabri <elgabriii97@gmail.com>
2022-02-14 16:51:49 +0000

    Troca da mensagem para 'HelloGitHub'

--- gitTeste/Program.cs
+++ gitTeste/Program.cs
@@ -6,7 +6,7 @@ namespace gitTeste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("HelloGitHub!");
         }
     }
 }
